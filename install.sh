05d2862a901732e7bdf94f01e48ed82a780a9b35
Shayne Hartford (ShayBox) <shayneehartford@gmail.com>
2020-08-06 18:05:51 -0400

    Reload and quiet

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 # Full URL used to prevent possible security attacks or deal urls
-INSERT='alias mon2cam="deno run --unstable --allow-run --allow-read --allow-env https://raw.githubusercontent.com/ShayBox/Mon2Cam/deno-rewrite/src/mod.ts"'
+INSERT='alias mon2cam="deno run --unstable --allow-run --allow-read --allow-env -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/deno-rewrite/src/mod.ts"'
 
 case $SHELL in
   /bin/bash)
@@ -18,4 +18,5 @@ case $SHELL in
   ;;
 esac
 
+echo "Installing alias for $SHELL"
 echo $INSERT >> $LOCATION
